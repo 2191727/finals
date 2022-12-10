b19d8bf2f0b961ce9632143ce7ff99bf71b02d36
Riel Victor Cezar <2191450@slu.edu.ph>
2022-12-10 18:00:57 +0800

    update

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
    <head>
       <meta charset="utf-8">
       <title>Login Page</title>
-      <link rel="stylesheet" href="/css/login.css">
+      <link rel="stylesheet" href="login.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
    <body>
@@ -20,9 +20,7 @@
          <div class="form-container">
             <div class="slide-controls">
                <input type="radio" name="slide" id="login" checked>
-               <input type="radio" name="slide" id="signup">
-               <label for="login" class="slide login">Login</label>
-               <label for="signup" class="slide signup">Signup</label>
+               <label for="login" class="slide login">Socials</label>
                <div class="slider-tab"></div>
             </div>
             <div class="form-inner">
@@ -33,38 +31,12 @@
                   <div class="field">
                      <input type="password" placeholder="Password" required>
                   </div>
-                  <div class="pass-link">
-                     <a href="#">Forgot password?</a>
-                  </div>
                   <div class="field btn">
                      <div class="btn-layer"></div>
                      <input type="submit" value="Login">
                   </div>
-                  <div class="signup-link">
-                     Not a member? <a href="">Signup now</a>
-                  </div>
                </form>
                <form action="#" class="signup">
-               <div class="field">
-                     <input type="text" placeholder="First Name" required>
-                  </div>
-                  <div class="field">
-                     <input type="text" placeholder="Last Name" required>
-                  </div>
-                  <div class="field">
-                     <input type="text" placeholder="Email Address" required>
-                  </div>
-                  <div class="field">
-                     <input type="password" placeholder="Password" required>
-                  </div>
-                  <div class="field">
-                     <input type="password" placeholder="Confirm password" required>
-                  </div>
-                  <div class="field btn">
-                     <div class="btn-layer"></div>
-                     <input type="submit" value="Signup">
-                  </div>
-               </form>
             </div>
          </div>
       </div>
@@ -81,10 +53,6 @@
          loginBtn.onclick = (()=>{
            loginForm.style.marginLeft = "0%";
            loginText.style.marginLeft = "0%";
-         });
-         signupLink.onclick = (()=>{
-           signupBtn.click();
-           return false;
          });
       </script>
    </body>
--- teacher.php
+++ teacher.php
@@ -4,7 +4,7 @@
    <head>
       <meta charset="utf-8">
       <title>Teacher</title>
-      <link rel="stylesheet" href="/css/teacher.css">
+      <link rel="stylesheet" href="teacher.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
    <body>
