8bc784ba9a1a46fbb6b6cb5d55a3a34cfc0dcea0
Riel Victor Cezar <2191450@slu.edu.ph>
2022-12-10 22:40:00 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Student</title>
+    <link rel="stylesheet" href="index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="https://kit.fontawsome.com/a076d05399"></script>
+</head>
+
+
+<body>
+
+    <div class="main-content">
+        <div class="header">
+            <h1>Header</h1>
+            <p>My supercool header</p>
+        </div>
+        <main>
+        </main>
+    </div>
+
+    <input type="checkbox" id="nav-toggle">
+    <div class="sidebar">
+        <div class="sidebar-student">
+
+            <div class="user-image">
+                <img src="team-socials.png" width="200px" height="100px" alt="">
+
+            </div>
+        </div>
+
+        <div class="sidebar-menu">
+            <ul class="nav">
+                <li>
+                    <a href="myexams.php"><span><i class="fa fa-users" aria-hidden="true"></i></span>
+                        <span>My Exams</span></a>
+                </li>
+                <li>
+                    <a href="showgrades.php"><span><i class="fa fa-list-alt" aria-hidden="true"></i></span>
+                        <span>Show Grades</span></a>
+                </li>
+                <li>
+                    <a href="login.php"><span><i class="fa fa-sign-out" aria-hidden="true"></i></span>
+                        <span>Logout</span></a>
+                </li>
+                <div class="active"></div>
+            </ul>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
---
+++ myexams.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Student</title>
+    <link rel="stylesheet" href="myexams.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="https://kit.fontawsome.com/a076d05399"></script>
+    <?php include 'index.php'; ?>
+</head>
+
+<body>
+    <div class="container">
+    <table class="content-table" style="width:1500px">
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Time Limit</th>
+                <th scope="col">Course</th>
+                <th scope="col">Status</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">CFE 135</th>
+                <td>5:00</td>
+                <td>CFE NA MALUPET</td>
+                <td>Not yet taken</td>
+                <td><button>Take Exam</button>
+                <button>View Result</button></td>
+            </tr>
+            <tr>
+            <th scope="row">CFE 135</th>
+                <td>5:00</td>
+                <td>CFE NA MALUPET</td>
+                <td>Not yet taken</td>
+                <td><button>Take Exam</button>
+                <button>View Result</button></td>
+            </tr>
+            <tr>
+            <th scope="row">CFE 135</th>
+                <td>5:00</td>
+                <td>CFE NA MALUPET</td>
+                <td>Not yet taken</td>
+                <td><button>Take Exam</button>
+                <button>View Result</button></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+
+</body>
+
+</html>
